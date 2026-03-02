c842601db648e2d1b977ac3c00a43392cc71379f
LuizBaldini <luizfelipebaldini.07@gmail.com>
2026-03-02 14:37:33 -0300

    First class position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        Position poss= new Position(3,5);
+        System.out.println(poss);
     }
 }
-q
