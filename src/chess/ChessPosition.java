c4980b7fc7fec1edef38e9daabda0a68bcc81618
LuizBaldini <luizfelipebaldini.07@gmail.com>
2026-03-03 14:03:37 -0300

    Little improvement in board printing

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -23,14 +23,14 @@ public class ChessPosition {
         return row;
     }
 
-    //Transforma o que o humano digitou no que o Java entende.
+    //Humano -> Java
     protected Position toPosition() {
         return new Position(8 - row, column - 'a');
     }
 
-    //Pega o que o Java calculou e mostra bonitinho pro humano.
+    //Java -> Humano
     protected static ChessPosition fromPosition(Position position) {
-        return new ChessPosition((char) ('a' - position.getColumn()), 8 - position.getRow());
+        return new ChessPosition((char) ('a' + position.getColumn()), 8 - position.getRow());
     }
 
     @Override
