fe98e512bba51842dbe2605bb6bf843137d8d057
LuizBaldini <luizfelipebaldini.07@gmail.com>
2026-03-07 17:42:33 -0300

    Fix - better treatment to promote

--- src/application/Program.java
+++ src/application/Program.java
@@ -22,7 +22,7 @@ public class Program {
 
             try {
                 UI.clearScreen();
-                UI.printMatch(chessMatch,captured);
+                UI.printMatch(chessMatch, captured);
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
@@ -36,13 +36,17 @@ public class Program {
                 ChessPosition target = UI.readChessPosition(sc);
 
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-                if(capturedPiece!= null){
+                if (capturedPiece != null) {
                     captured.add(capturedPiece);
                 }
 
-                if(chessMatch.getPromoted()!=null){
+                if (chessMatch.getPromoted() != null) {
                     System.out.print("Enter piece for promotion (B/N/R/Q): ");
-                    String type = sc.nextLine();
+                    String type = sc.nextLine().toUpperCase();
+                    while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+                        System.out.println("Invalid value! Enter piece for promotion (B/N/R/Q): ");
+                        type = sc.nextLine().toUpperCase();
+                    }
                     chessMatch.replacePromotedPiece(type);
                 }
             } catch (ChessException e) {
@@ -55,6 +59,6 @@ public class Program {
             }
         }
         UI.clearScreen();
-       UI.printMatch(chessMatch,captured);
+        UI.printMatch(chessMatch, captured);
     }
 }
